baa6656fa758df9e114ae5ef5f98a9de6ed0bc30
hippobit <support@hippobit.dev>
2020-02-07 22:36:23 -0800

    Footer done.

--- hippobit_react/src/components/footer.js
+++ hippobit_react/src/components/footer.js
@@ -1,17 +1,27 @@
 import React from "react"
-import { Nav, Navbar, Row, Container, Col } from "react-bootstrap"
+import { Row, Container, Col } from "react-bootstrap"
+import fbIcon from "../images/facebook.svg"
+import footerCss from "./footer.module.css"
 
 const footer = () => (
-  <Container fluid className="bg-dark">
-    <Row>
+  <Container fluid className={"bg-dark " + footerCss.container}>
+    < Row >
       <Col>
         <small className="text-light">
           © {new Date().getFullYear()} Hippo Bit. All rights reserved.
         </small>
       </Col>
-      <Col>fb</Col>
-    </Row>
-  </Container>
+      <Col className="d-flex justify-content-end" >
+        <a href="https://www.facebook.com/hippobit">
+          <img
+            className={footerCss.icon}
+            src={fbIcon}
+            alt="facebook"
+          />
+        </a>
+      </Col>
+    </Row >
+  </Container >
 )
 
 export default footer
